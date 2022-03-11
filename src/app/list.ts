9c790124420cf11478a56fac6ef37bc4b1012200
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-03-11 20:40:10 -0300

    1 file modified

--- src/app/list.ts
+++ src/app/list.ts
@@ -1,5 +1,9 @@
 export const listData = [
-  {id: 1, text: "Texto Um"},
-  {id: 2, text: "Texto Dois"},
-  {id: 3, text: "Texto Três"}
+  {id: 1, text: "Desenvolvimento para Servidores II"},
+  {id: 2, text: "Desenvolvimento de Dispositivos Movéis I"},
+  {id: 3, text: "Negócios, Marketing e Eletrônicos"},
+  {id: 4, text: "Projeto de Prototipagem e Testes de Usabilidade"},
+  {id: 5, text: "Tópicos Especiais II"},
+  {id: 6, text: "DP - Desenvolvimento para Servidores I"},
+  {id: 7, text: "DP - Banco de Dados II"},
 ]
