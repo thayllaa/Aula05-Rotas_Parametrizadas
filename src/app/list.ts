f546e7f08ac40652a8968068343c763598065f3f
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-03-11 20:55:16 -0300

    2 files modified

--- src/app/list.ts
+++ src/app/list.ts
@@ -1,9 +1,9 @@
 export const listData = [
-  {id: 1, text: "Desenvolvimento para Servidores II"},
-  {id: 2, text: "Desenvolvimento de Dispositivos Movéis I"},
-  {id: 3, text: "Negócios, Marketing e Eletrônicos"},
-  {id: 4, text: "Projeto de Prototipagem e Testes de Usabilidade"},
-  {id: 5, text: "Tópicos Especiais II"},
-  {id: 6, text: "DP - Desenvolvimento para Servidores I"},
-  {id: 7, text: "DP - Banco de Dados II"},
+  {id: 1, text: "Banco de Dados II - DP", date: "Quarta-feira", hour: "9h30 - 13h"},
+  {id: 2, text: "Desenvolvimento para Servidores I - DP", date: "Segunda-feira", hour: "9h30 - 13h"},
+  {id: 3, text: "Desenvolvimento para Servidores II", date: "Quinta-feira", hour: "9h30 - 13h"},
+  {id: 4, text: "Desenvolvimento de Dispositivos Movéis I", date: "Segunda-feira", hour: "9h30 - 13h"},
+  {id: 5, text: "Negócios, Marketing e Eletrônicos", date: "Quarta-feira", hour: "9h30 - 13h"},
+  {id: 6, text: "Projeto de Prototipagem e Testes de Usabilidade", date: "Sábado", hour: "9h30 - 13h"},
+  {id: 7, text: "Tópicos Especiais II", date: "Sexta-feira", hour: "9h30 - 13h"}
 ]
