5abbfafb081cc9d9ac6c9b1e105cfcf2fb7ca7e0
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-03-11 22:33:56 -0300

    1 file modified

--- src/app/list.ts
+++ src/app/list.ts
@@ -1,5 +1,6 @@
 export const listData = [
-  {id: 1, text: "Banco de Dados II - DP", date: "Quarta-feira", hour: "9h30 - 13h"},
+  {id: 1, text: "Banco de Dados II - DP", date: "Quarta-feira", hour: "9h30 - 13h",
+content: "Objetivo - Apresentar recursos avançados para criação de aplicativos com Banco de dados. <br> Ementa - Tipos de Junções (join). Subconsultas (Subqueries). Criação, implementação e utilização de procedimentos armazenados (stored procedures), gatilhos (triggers), funções, cursores, visões (view). Linguagem de Controle de Dados. Transações. Bancos de dados orientados a objetos. Bancos de dados multimídia."},
   {id: 2, text: "Desenvolvimento para Servidores I - DP", date: "Segunda-feira", hour: "9h30 - 13h"},
   {id: 3, text: "Desenvolvimento para Servidores II", date: "Quinta-feira", hour: "9h30 - 13h"},
   {id: 4, text: "Desenvolvimento de Dispositivos Movéis I", date: "Segunda-feira", hour: "9h30 - 13h"},
